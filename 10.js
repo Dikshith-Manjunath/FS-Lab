5f8a1bf49c177709ee8dac89f70146b55261432e
Dikshith Manjunath <dikshithrrddy@gmail.com>
2026-04-30 08:14:42 +0530

    First Commit

--- 10.js
+++ 10.js
@@ -1,28 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
-const Undertaking = require("./models/Undertaking");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
-mongoose.connect("mongodb://127.0.0.1:27017/undertakingDB")
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => console.log(err));
+mongoose.connect("mongodb://127.0.0.1/examDB");
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+const Form = mongoose.model("Form", { 
+    name: String, usn: String, dept: String, reason: String, date: String, sign: String 
 });
 
 app.post("/submit", async (req, res) => {
-    try {
-        const formData = new Undertaking(req.body);
-        await formData.save();
-        res.send("<h2>Submitted Successfully!</h2><a href='/'>Go Back</a>");
-    } catch (error) {
-        res.send("Error: " + error.message);
-    }
+    await new Form(req.body).save();
+    res.send("Saved Successfully!");
 });
 
-app.listen(3000, () => console.log("Server running on http://localhost:3000"));
+app.listen(3000, () => console.log("Running..."));
